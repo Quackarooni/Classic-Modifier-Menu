e60ae25732207d24ab7dff5612040e5bbe9d6951
Quackers <shadowrogue624@gmail.com>
2024-11-08 20:01:25 +0800

    Keymaps: Make compatible with Blender 4.3

--- keymaps.py
+++ keymaps.py
@@ -1,39 +1,51 @@
+import bpy
+
 from .keymap_ui import KeymapItemDef, KeymapStructure, KeymapLayout
-from .operators import (
-    INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU, 
-    INVOKE_OT_CLASSIC_MODIFIER_MENU, 
-    INVOKE_OT_ASSET_MODIFIER_MENU, 
-    INVOKE_OT_ADD_GPENCIL_MODIFIER_MENU, 
-    INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU,
-    INVOKE_OT_ADD_CONSTRAINTS_MENU,
-    INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU,
-    )
+
+if bpy.app.version <= (4, 3, 0):
+    from .operators import (
+        INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU, 
+        INVOKE_OT_CLASSIC_MODIFIER_MENU, 
+        INVOKE_OT_ASSET_MODIFIER_MENU, 
+        INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU,
+        INVOKE_OT_ADD_CONSTRAINTS_MENU,
+        INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU,
+        )
+else:
+    from .operators import (
+        INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU, 
+        INVOKE_OT_CLASSIC_MODIFIER_MENU, 
+        INVOKE_OT_ASSET_MODIFIER_MENU, 
+        INVOKE_OT_ADD_GPENCIL_MODIFIER_MENU, 
+        INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU,
+        INVOKE_OT_ADD_CONSTRAINTS_MENU,
+        INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU,
+        )
 
 keymap_info = {
     "keymap_name" : "Property Editor",
     "space_type" : "PROPERTIES",
 }
 
-keymap_defs = (
-    (INVOKE_OT_CLASSIC_MODIFIER_MENU.bl_idname, 'A', True, None),
-    (INVOKE_OT_ASSET_MODIFIER_MENU.bl_idname, 'NONE', False, None),
-    (INVOKE_OT_ADD_GPENCIL_MODIFIER_MENU.bl_idname, 'A', True, None),
-    (INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU.bl_idname, 'A', True, None),
-    (INVOKE_OT_ADD_CONSTRAINTS_MENU.bl_idname, 'A', True, None),
-    (INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU.bl_idname, 'A', True, None),
-)
-
-
-keymap_structure = KeymapStructure([
-    KeymapItemDef(INVOKE_OT_CLASSIC_MODIFIER_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
-    KeymapItemDef(INVOKE_OT_ASSET_MODIFIER_MENU.bl_idname, **keymap_info),
-    KeymapItemDef(INVOKE_OT_ADD_GPENCIL_MODIFIER_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
-    KeymapItemDef(INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
-    KeymapItemDef(INVOKE_OT_ADD_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
-    KeymapItemDef(INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
-    ]
-)
-
+if bpy.app.version <= (4, 3, 0):
+    keymap_structure = KeymapStructure([
+        KeymapItemDef(INVOKE_OT_CLASSIC_MODIFIER_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ASSET_MODIFIER_MENU.bl_idname, **keymap_info),
+        KeymapItemDef(INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ADD_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        ]
+    )
+else:
+    keymap_structure = KeymapStructure([
+        KeymapItemDef(INVOKE_OT_CLASSIC_MODIFIER_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ASSET_MODIFIER_MENU.bl_idname, **keymap_info),
+        KeymapItemDef(INVOKE_OT_ADD_GPENCIL_MODIFIER_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ADD_GPENCIL_SHADERFX_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ADD_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        KeymapItemDef(INVOKE_OT_ADD_BONE_CONSTRAINTS_MENU.bl_idname, **keymap_info, shift=True, key_type='A'),
+        ]
+    )
 
 keymap_layout = KeymapLayout(layout_structure=keymap_structure)
 
