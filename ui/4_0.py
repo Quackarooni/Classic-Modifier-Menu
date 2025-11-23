c28d412ca74f0d904947b41bc921f1b95d89bbc0
Quackers <quackadoobers@gmail.com>
2025-11-23 20:10:12 +0800

    4.0: Remove "Add Modifier" button for Hair Curve Object since menu is empty

--- ui/4_0.py
+++ ui/4_0.py
@@ -51,12 +51,14 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
             sublayout = layout.row()
 
         if prefs.display_as == "DROPDOWN":
-            sublayout.menu("OBJECT_MT_modifier_add", text=modifier_label)
+            if ob_type != 'CURVES' and bpy.app.version < (4, 2):
+                sublayout.menu("OBJECT_MT_modifier_add", text=modifier_label)
             if prefs.show_assets:
                 sublayout.menu("OBJECT_MT_modifier_add_assets", text=asset_label)
                 
         elif prefs.display_as == "BUTTON":
-            sublayout.operator("object.invoke_classic_modifier_menu", text=modifier_label, icon='ADD')
+            if ob_type != 'CURVES' and bpy.app.version < (4, 2):
+                sublayout.operator("object.invoke_classic_modifier_menu", text=modifier_label, icon='ADD')
             if prefs.show_assets and geometry_nodes_supported(ob_type):
                 sublayout.operator("object.invoke_asset_modifier_menu", text=asset_label, icon='ADD')
 
